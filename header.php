5d3c77a1556259efd0c75d5790aaede279a67e04
Damian Dzieduch <d.dzieduch@gmail.com>
2018-08-29 21:26:06 +0200

    menu modification - audit

--- header.php
+++ header.php
@@ -89,13 +89,13 @@ $src = get_template_directory_uri();
 			</div>
 			<div class="row">
 				<div class="small-5 columns show-for-large">
-					<?php wp_nav_menu(array('theme_location' => 'primary', 'menu_class' => 'text-center main-menu')); ?>
+					<?php wp_nav_menu(array('theme_location' => 'primary', 'menu_class' => 'text-right main-menu')); ?>
 				</div>
 				<div class="small-6 large-2 columns large-text-center">
 					<a href="<?php echo home_url(); ?>"><img src="<?php echo $src; ?>/images/logo.png" alt="<?php bloginfo( 'name' ); ?>" /></a>
 				</div>
 				<div class="small-5 columns show-for-large relative">
-					<?php wp_nav_menu(array('theme_location' => 'primary2', 'menu_class' => 'text-center main-menu')); ?>
+					<?php wp_nav_menu(array('theme_location' => 'primary2', 'menu_class' => 'text-left main-menu')); ?>
 					<div class="lang">
 						<?php
 						$langs = icl_get_languages('skip_missing=0');
